e05d1ab1f119dd548989491cd1f85ba4b1ff96e7
Clive Chan <cc@clive.io>
2016-11-20 17:47:36 -0500

    generalize slightly to allow other branches

--- uphook.js
+++ uphook.js
@@ -19,11 +19,13 @@ module.exports.github = function(secret, callback){
     //returns two middlewares for Express to run sequentially.
     ghHandler,
     function (err, req, res, next){
-      if(req.headers['x-github-event'] != 'push' || req.body.ref != 'refs/heads/' + gitbranch.sync())
+      let branch = gitbranch.sync();
+      if(req.headers['x-github-event'] != 'push' || req.body.ref != 'refs/heads/' + branch)
         res.sendStatus(200);
       else
         //Careful, as this erases all tracked changes.
-        exec('git fetch --all && git reset --hard origin/master && npm install && npm prune')
+        //todo: generalize to default to tracked branch but be able to pull from any remote
+        exec('git fetch --all && git reset --hard origin/' + branch + ' && npm install && npm prune')
         .spread(function(stdout, stderr){
           console.log(stdout, stderr);
           res.sendStatus(200);
