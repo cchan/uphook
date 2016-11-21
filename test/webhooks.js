8f26d08260b712d7687cff39a4c6f8fd3d502fd6
Clive Chan <cc@clive.io>
2016-11-20 21:40:17 -0500

    generalized the code, a few bugfixes, and trying to unit test

--- test/webhooks.js
+++ test/webhooks.js
@@ -1,18 +1,25 @@
+'use strict';
+
 const secret = 'superawesomesecret';
 
+let request = require('supertest');
 let express = require('express');
 let app = express();
 let uphook = require('../uphook');
+let fs = require('fs');
 
-app.use('gh-hook', uphook);
+app.post('/gh-hook', uphook.github({secret: secret}));
 
 describe('github webhooks', function() {
   it('takes a valid GitHub webhook', function(done) {
-    request(app)
+    request(app, require('./test/assets/github-valid.json'))
       .post('/gh-hook')
-      .set('Accept', 'application/json')
-      //hardcoded hmac digest headers
-      .expect('Content-Type', /json/)
+      .set('content-type', 'application/json')
+      .set('Expect', '')
+      .set('User-Agent', 'GitHub-Hookshot/ac156fb')
+      .set('X-GitHub-Delivery', 'd1af9a80-af80-11e6-9c78-b4baecc83d2a')
+      .set('X-GitHub-Event', 'push')
+      .set('X-Hub-Signature', 'sha1=6104fc634954b1ca0fc835300d71b790829489bc')
       .expect(200, done);
   });
 });
--- uphook.js
+++ uphook.js
@@ -3,58 +3,94 @@ uphook
 express middleware to automatically update and restart an application based on github webhook.
 
 usage: if verified hook on right branch, git force-pulls to current branch and calls the callback.
-  let hook = require('uphook').github(process.env.GH_SECRET, function(payload){
-    process.exit(0); //lets PM2 restart the process
+  let hook = require('uphook').github({
+    secret: process.env.GH_SECRET,
+    verify: function(req, res){
+      //before it calls gitupdate(), can return false to cancel that.
+    },
+    callback: function(err, payload){
+      if(!err)
+        process.exit(0); //lets PM2 restart the process
+    }
   });
   express.use('/gh-update', hook);
 
 note that you should definitely use SSL because GitLab doesn't have HMAC, which is dumb.
 */
 
+'use strict';
+
 let bluebird = require('bluebird');
 let exec = bluebird.promisify(require('child_process').exec);
 let branch = require('git-branch').sync(); //meh, it could change the branch mid-execution but like that's even more problematic
 let bodyParser = require('body-parser');
+let crypto = require('crypto');
+let fs = require('fs');
 
 //Careful, as this erases all tracked changes.
 //todo: generalize to default to tracked branch but be able to pull from any remote
 function gitupdate(req, res, callback){
-  exec('git fetch --all && git reset --hard origin/' + branch + ' && npm install && npm prune')
+  let promise = exec('git fetch --all && git reset --hard origin/' + branch + ' && npm install && npm prune')
   .spread(function(stdout, stderr){
     console.log(stdout, stderr);
     res.sendStatus(200);
-    callback(req.body);
-  }).catch(function(err){
-    console.error(err);
-    res.sendStatus(500);
+    callback(null, req.body);
   });
+  
+  if(callback)
+    return promise.catch(function(err){
+      console.error(err);
+      res.sendStatus(500);
+      callback(err, req.body);
+    });
+  else
+    return promise;
 }
 
-module.exports.github = function(secret, callback){
-  let ghHandler = require('github-webhook-middleware')({secret: secret});
+function processArgs(options){
+  let secret = options.secret;
+  if(!secret) throw new Error('No secret given');
+  
+  let verify = options.verify || function(){return true;}
+  let callback = options.callback || function(){};
+  
+  return {secret, verify, callback};
+}
+
+let generalized = function(options, checkHeaders){
+  {secret, verify, callback} = processArgs(options);
+  
   return [
-    //returns two middlewares for Express to run sequentially.
-    ghHandler,
-    function (err, req, res, next){
-      if(req.headers['x-github-event'] != 'push' || req.body.ref != 'refs/heads/' + branch)
-        res.sendStatus(200);
+    bodyParser.json({
+      verify: function(req, res, buffer){
+        req.digest = function(){
+          return 'sha1=' + crypto.createHmac('sha1', secret).update(buffer).digest('hex');
+        };
+      }
+    }),
+    function (req, res, next){
+      if(!checkHeaders(req))
+        res.sendStatus(403);
+      else if(!verify(req, res))
+        res.sendStatus(500);
       else
         gitupdate(req, res, callback);
     }
   ];
+}
+
+module.exports.github = function(options){
+  return generalized(options, function(req){
+    return (req.headers['x-hub-signature'] == req.digest()
+      && req.headers['x-github-event'] == 'push'
+      && req.body.ref == 'refs/heads/' + branch);
+  });
 };
 
-module.exports.gitlab = function(secret, callback){
-  return bodyParser.json({
-    verify: function (req, res, buffer){
-      //no hmac, smh gitlab
-      //'sha1=' + crypto.createHmac('sha1', secret).update(buffer).digest('hex') == req.headers['x-gitlab-signature'];
-      if(req.headers['x-gitlab-token'] != secret)
-        throw new Error('Invalid Token');
-      else if(req.headers['x-gitlab-event'] != 'Push Hook' || req.body.ref != 'refs/heads/' + branch)
-        res.sendStatus(200);
-      else
-        gitupdate(req, res, callback);
-    }
+module.exports.gitlab = function(options){
+  return generalized(options, function(req){
+    return (req.headers['x-gitlab-token'] == secret
+      && req.headers['x-gitlab-event'] == 'Push Hook'
+      && req.body.ref == 'refs/heads/' + branch);
   });
 };
