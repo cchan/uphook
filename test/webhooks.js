8d9dde126fd65998749b63f79572e4707e7bdd74
Clive Chan <cc@clive.io>
2016-11-20 22:39:36 -0500

    tests aren't passing because of silly json reading stuff, but it does work

--- test/webhooks.js
+++ test/webhooks.js
@@ -8,18 +8,21 @@ let app = express();
 let uphook = require('../uphook');
 let fs = require('fs');
 
-app.post('/gh-hook', uphook.github({secret: secret}));
+app.post('/gh-hook', uphook.github({
+  secret: secret,
+  verify: function(){return false;}
+}));
 
 describe('github webhooks', function() {
   it('takes a valid GitHub webhook', function(done) {
-    request(app, require('./test/assets/github-valid.json'))
+    request(app, new Buffer(fs.readFileSync('./test/assets/github-valid.json'), 'base64'))
       .post('/gh-hook')
       .set('content-type', 'application/json')
       .set('Expect', '')
       .set('User-Agent', 'GitHub-Hookshot/ac156fb')
       .set('X-GitHub-Delivery', 'd1af9a80-af80-11e6-9c78-b4baecc83d2a')
       .set('X-GitHub-Event', 'push')
-      .set('X-Hub-Signature', 'sha1=6104fc634954b1ca0fc835300d71b790829489bc')
-      .expect(200, done);
+      .set('X-Hub-Signature', 'sha1=eb1ea81820c5868b50aae70cc5b702b791b962ca')
+      .expect(500, done);
   });
 });
